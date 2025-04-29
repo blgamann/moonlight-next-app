fdf3df0f4545e6dbc80841ecebbd4463ab3264f2
0xdagarn <sgnoo@naver.com>
2025-04-29 21:46:10 +0900

    chore: adjust spaces

--- app/page.tsx
+++ app/page.tsx
@@ -21,12 +21,12 @@ export default function Home() {
         <TextGrey className="text-[22px] mt-12 font-medium">
           독후감을 공유하고 나의 소울 메이트를 찾아보세요
         </TextGrey>
-        <BsMoonFill color="#39d4e7" size={14} className="mt-20" />
-        <TextBlack className="text-[22px] mt-16 mb-12 font-semibold">
+        <BsMoonFill color="#39d4e7" size={14} className="mt-18" />
+        <TextBlack className="text-[22px] mt-18 mb-12 font-semibold">
           어떤 분이 추천해주셨나요?
         </TextBlack>
         <Input placeholder="추천인의 ID를 입력해주세요" />
-        <Button className="mt-16">회원가입 시작하기</Button>
+        <Button className="mt-17">회원가입 시작하기</Button>
       </div>
     </div>
   );
